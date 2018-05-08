9e88590c3e60e523d9639fb75fa92a44a030c139
Nolan <nemerson17@gmail.com>
2018-05-08 12:27:59 -0700

    Receives info from Firebase properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import base from '../base';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      locationNames: []
+    }
+  }
+
+  componentDidMount() {
+    base.fetch(`/locations`, { context: this }).then( response => {
+      console.log('Response: ', response);
+      this.setupLocations(response);
+    });
+    // console.log(this.props);
+    // this.ref = base.syncState()
+  }
+
+  setupLocations(locations) {
+    const locationNames = Object.keys(locations);
+
+    this.setState({
+      locationNames
+    });
+  }
+
   render() {
+
+    const locationMap = this.state.locationNames.map( (item, index) => {
+      return <div>{item}</div>
+    });
+
     return (
       <div className='mainContainer'>
         <Header />
-        Suh
+        {locationMap}
       </div>
     );
   }
