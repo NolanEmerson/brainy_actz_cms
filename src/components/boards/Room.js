f2106d3d67288947e53cd6b9d23768f51b18396e
Nolan <nemerson17@gmail.com>
2018-05-15 12:27:35 -0700

    Added two new room roughdrafts

---
+++ src/components/boards/Room.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../css/boards.css';
+
+export default props => {
+    return (
+        <div className="roomBoard">
+            <h1 className="roomBoardTitle">Room Title</h1>
+            <h2 className="roomBoardSubtitle">Room Subtitle</h2>
+        </div>
+    );
+}
---
+++ src/components/boards/Text.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../css/boards.css';
+
+export default props => {
+    return (
+        <div className="textBoard">
+            <h1 className="textBoardTitle">Title</h1>
+            <h2 className="textBoardSubtitle">Subtitle</h2>
+        </div>
+    );
+}
