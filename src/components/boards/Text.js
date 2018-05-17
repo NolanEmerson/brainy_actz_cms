af262f8134451c766d072e3bbedc704d8f947e15
Nolan <nemerson17@gmail.com>
2018-05-17 13:38:24 -0700

    Slight changes on how to send/retrieve text

--- src/components/boards/Text.js
+++ src/components/boards/Text.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../css/boards.css';
 
 export default props => {
+    
     return (
         <div className="textBoard">
             <h1 className="textBoardTitle">{props.title || 'Placeholder title'}</h1>
